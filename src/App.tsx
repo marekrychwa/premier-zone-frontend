b16fb88d6b8ac16b8619b2f9ef0f527cb19f83e6
marekrychwa <marek.rychwa@gmail.com>
2026-01-06 22:21:46 +0100

    Initial React frontend setup with layout and navbar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="container">
+        <h2>Welcome to PremierZone</h2>
+      </main>
+    </>
+  );
+}
